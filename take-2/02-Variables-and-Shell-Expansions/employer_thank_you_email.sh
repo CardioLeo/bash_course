63a809e337f925eb82a1ccd8971624d94dab23ac
CardioLeo <CardioLeo@gmail.com>
2025-04-11 22:35:11 -0600

    adds two scripts that illustrate variables and parameter expansion - parameter expansion sounds so much more complex when it's not just phrased as, referecing variables, etc.

---
+++ take-2/02-Variables-and-Shell-Expansions/employer_thank_you_email.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+employee="Bob"
+
+echo "Dear ${employee}"
+echo "The people and I would like to thank you for the compassionate and thoughtful work that you have been doing over the past months."
+echo "Keep up the good work, ${employee}"
+
+echo "All the best,"
+echo "Sarah"
