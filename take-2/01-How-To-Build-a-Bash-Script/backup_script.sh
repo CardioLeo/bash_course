d879de3d0f979a88a9ccfa68423e84909f81b905
CardioLeo <CardioLeo@gmail.com>
2025-04-11 21:48:54 -0600

    creates sh script doc with first two lines, plus some basic comments

---
+++ take-2/01-How-To-Build-a-Bash-Script/backup_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Author: William Z Chadwick
+# Date Created: 04-11-2025
+# Usage: backup_script.sh
+# Description:
+# Backs up files within the path given
+#
+
+# the main tar command - although I probably need to change the path
+tar -cvf ~/bash_course/my_backup_"$(date +%d-%m-%Y-%H-%M-%S)".tar ~/* 2>/dev/null
