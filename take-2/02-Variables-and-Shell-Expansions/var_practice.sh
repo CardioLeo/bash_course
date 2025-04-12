a35a4695997bff7ba3e350c5d637030463dfc831
CardioLeo <CardioLeo@gmail.com>
2025-04-11 22:29:27 -0600

    adds var_practice.sh script; which has one variable and echoes it to the terminal

---
+++ take-2/02-Variables-and-Shell-Expansions/var_practice.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+student="Sarah"
+
+# no spaces or it will interpret them as different commands
+# no uppercase or it may look like a shell variable rather than a user variable
+
+echo "Hello, ${student}"
